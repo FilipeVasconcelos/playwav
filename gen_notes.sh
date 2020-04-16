37f5ddd3d849b8c9c39bb146d737da6ba7e5abc9
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-04-16 21:54:46 +0200

    octaves

--- gen_notes.sh
+++ gen_notes.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 notes=("do" "doD" "ré" "réD" "mi" "fa" "faD" "sol" "solD" "la" "laD" "si")
 freq=("523.3" "554.4" "587.3" "622.3" "659.3" "698.5" "740.0" "784.0" "830.6" "880.0" "932.3" "987.8")
+octaves=("0.125" "0.25" "0.5" "1" "2" "4" "8" "16" "32" "64")
 n=${#notes[*]}
-for temps in "N" "B"
+for octave in 2 4 
 do
-    if [ ${temps} == "B" ]
-    then
-        ktemps=16
-        valtemps=-6
-    else
-        ktemps=15
-        valtemps=-7.5
-    fi
-    for i in $(seq 0 $((n-1)))
+    for temps in "N" "B"
     do
-        echo ${i} ${notes[${i}]} ${note} ${temps}
-        python3 generate_1d_signal.py -p ${ktemps} -fs 44100 -s realexp -d ${freq[${i}]} 0 ${valtemps} -w ${notes[${i}]}${temps}.wav
+        if [ ${temps} == "B" ]
+        then
+            ktemps=16
+            valtemps=-6
+        else
+            ktemps=15
+            valtemps=-7.5
+        fi
+        for i in $(seq 0 $((n-1)))
+        do
+            echo ${i} ${notes[${i}]} ${note} ${temps}
+            freqo=$(echo "scale=5;${freq[${i}]}*${octaves[${octave}]}"|bc)
+            python3 generate_1d_signal.py -p ${ktemps} -fs 44100 -s realexp -d ${freqo} 0 ${valtemps} -w ${notes[${i}]}${temps}_O${octave}.wav
+            mplayer ${notes[${i}]}${temps}_O${octave}.wav
+        done
     done
 done
 
--- playkey.sh
+++ playkey.sh
@@ -4,27 +4,27 @@ while true;
 do
     read -rsn1 input
     if [ "$input" = "a" ]; then
-        mplayer "doN.wav"
+        mplayer "doN_O3.wav"
     fi
     if [ "$input" = "q" ]; then
-        mplayer "réN.wav"
+        mplayer "réN_O3.wav"
     fi
     if [ "$input" = "w" ]; then
-        mplayer "miN.wav"
+        mplayer "miN_O3.wav"
     fi
     if [ "$input" = "z" ]; then
-        mplayer "faN.wav"
+        mplayer "faN_O3.wav"
     fi
     if [ "$input" = "s" ]; then
-        mplayer "solN.wav"
+        mplayer "solN_O3.wav"
     fi
     if [ "$input" = "x" ]; then
-        mplayer "laN.wav"
+        mplayer "laN_O3.wav"
     fi
     if [ "$input" = "e" ]; then
-        mplayer "siN.wav"
+        mplayer "siN_O3.wav"
     fi
     if [ "$input" = "d" ]; then
-        mplayer "doN1.wav"
+        mplayer "doN_O4.wav"
     fi
 done 
