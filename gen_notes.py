ebf3730af0e18d3e349cd25b9272523bc07c2f9f
FilipeVasconcelos <filipe.manuel.vasconcelos@gmail.com>
2020-04-18 12:29:01 +0200

    trois voix sur satie

--- gen_notes.py
+++ gen_notes.py
@@ -10,9 +10,10 @@ from scipy.io import wavfile
 # ------------------------------------------------------------------------------
 fs=float(44100) #Hz fréquence d'échantionnage
 ts=1./fs
-AMPLITUDE=10**5 
-
-key_freq={"do"  :261.6,
+AMPLITUDE=10**6
+# ------------------------------------------------------------------------------
+key_freq={""    :0.0,
+          "do"  :261.6,
           "doD" :277.2, 
           "re"  :293.7, 
           "reD" :311.1, 
@@ -25,29 +26,31 @@ key_freq={"do"  :261.6,
           "sib" :466.2, 
           "si"  :493.9
           }
-
+# ------------------------------------------------------------------------------
 octavesCoeff=[0.125]
 for i in range(1,10):
     octavesCoeff.append(octavesCoeff[-1]*2)
-
-dureeData={"d4":(17,-3),"d3":(16.5,-3),"d2":(16,-6),"d1":(15,-5),"d0":(14,-10) }
-
 # ------------------------------------------------------------------------------
-def realexp(freq,ph,ts,N,a):
+dureeData={"d4":(17,-3),"d3":(16.585,-2),"d2":(16,-4),"d1":(15,-6),"d0":(14,-10) }
+# ------------------------------------------------------------------------------
+def signal(freq,ph,ts,N,a):
     tpi=2.*np.pi
     xe=np.zeros(N,dtype=np.complex_)
     for k in range(N):
-        xe[k]=((k*ts)**0.08)*np.sin(freq*tpi*k*ts+ph)*np.exp(a*(k*ts))
+        xe[k]=((k*ts)**0.1)*np.sin(freq*tpi*k*ts+ph)*np.exp(a*(k*ts))
+    #    xe[k]=np.sin(freq*tpi*k*ts+ph)*np.exp(a*(k*ts))
     return xe
-
 # ------------------------------------------------------------------------------
+# name = keyo3dx
+# key : si solD laD
+# oi  : i = 0 à 9
+# dx  : Durée x = "B" blanche ou "N" noire "C"
 def extractInfoFromName(name):
     duree=name[-2:]
     octave=name[-3:-2]
     nc=len(name)-4
     key=name[:nc]
     return key,duree,int(octave)
-
 # ------------------------------------------------------------------------------
 def maxdureeNotes(notes):
     maxd="d0"
@@ -56,35 +59,37 @@ def maxdureeNotes(notes):
         if dureeData[d][0] > dureeData[maxd][0] :
             maxd=d
     return maxd
-
 # ------------------------------------------------------------------------------
-# name = keyo3dx
-# key : si solD laD
-# oi  : i = 0 à 9
-# dx  : Durée x = "B" blanche ou "N" noire "C"
-def genNote(notes,plotNote=False,writeWav=False,playWav=False):
+def genNote(notes,plotNote=False,writeWav=False,playWav=False,verbose=0):
     maxd=maxdureeNotes(notes)
     N=int(2**dureeData[maxd][0])
     xe=np.zeros(N,dtype=np.complex_)
-    print(60*'=')
-    print("INFO : number of sample points" ,N)
-    print("INFO : sampling frequency (Hz)" ,fs)
-    print("INFO : signal duration    (s)"  ,ts*N)
+    if verbose > 10 :
+        print(60*'=')
+        print("INFO : number of sample points" ,N)
+        print("INFO : sampling frequency (Hz)" ,fs)
+        print("INFO : signal duration    (s)"  ,ts*N)
     notename=''
-    for note in notes:
+    phas=0.0
+    for knote,note in enumerate(notes):
         notename+=note
         k,d,o=extractInfoFromName(note)
-        phas=0.0
         a=dureeData[d][1]
         freq=key_freq[k]*octavesCoeff[o]
-        print(60*'-')
-        print("INFO : frequence key      (Hz)" ,freq)
-        print("INFO : phase     key      ()"   ,phas)
-        print("INFO : damping   key      (Hz)" ,a)
-        xe+=realexp(freq,phas,ts,N,a)
+        if verbose > 10 :
+            print(60*'-')
+            print("INFO : frequence key      (Hz)" ,freq)
+            print("INFO : phase     key      ()"   ,phas)
+            print("INFO : damping   key      (Hz)" ,a)
+        xe+=signal(freq,phas,ts,N,a)
+        phas+=90
     if plotNote :
         t=np.linspace(0,ts*N,N)
-        plt.plot(t,xe.real)
+        fig, ax = plt.subplots(1, 1)
+        ax.plot(t,xe.real)
+        ax.set_title(notes)
+        ax.set_xlabel('time [s]')
+        ax.set_ylabel('Amplitude ')
         plt.show()
     if writeWav :
         notenamefile=notename+".wav"
@@ -145,31 +150,111 @@ if __name__ =="__main__":
            ["laO3d2","solO2d2"]
         #-----------------------------------------
           ]
+    satie_voix1=[
+        #-----------------------------------------
+           ["O0d1"],
+           ["siO2d2","reO3d2","faDO3d2"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["laO2d2","doDO3d2","faDO3d2"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["siO2d2","reO3d2","faDO3d2"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["laO2d2","doDO3d2","faDO3d2"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["siO2d2","reO3d2","faDO3d2"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["laO2d2","doDO3d2","faDO3d2"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["siO2d2","reO3d2","faDO3d2"],
+        #-----------------------------------------
+           ["O0d3"],
+          ]
+    satie_voix2=[
+        #-----------------------------------------
+           ["solO2d3"],
+        #-----------------------------------------
+           ["reO2d3"],
+        #-----------------------------------------
+           ["solO2d3"],
+        #-----------------------------------------
+           ["reO2d3"],
+        #-----------------------------------------
+           ["solO2d3"],
+        #-----------------------------------------
+           ["reO2d3"],
+        #-----------------------------------------
+           ["solO2d3"],
+        #-----------------------------------------
+           ["reO2d3"]
+          ]
+    satie_voix3=[
+        #-----------------------------------------
+           ["O0d1"],
+           ["O0d1"],
+           ["O0d1"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["O0d1"],
+           ["O0d1"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["O0d1"],
+           ["O0d1"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["O0d1"],
+           ["O0d1"],
+        #-----------------------------------------
+           ["O0d1"],
+           ["faDO4d1"],
+           ["laO4d1"],
+        #-----------------------------------------
+           ["solO4d1"],
+           ["faDO4d1"],
+           ["doDO4d1"],
+        #-----------------------------------------
+           ["siO3d1"],
+           ["doDO4d1"],
+           ["reO4d1"],
+        #-----------------------------------------
+           ["laO3d3"]
+          ]
 
+    piecename="satie.wav"
+    tracknamefiles=""
+    tracknames=["satie_voix1.wav","satie_voix2.wav","satie_voix3.wav"]
+    for ktrack,track in enumerate([satie_voix1,satie_voix2,satie_voix3]):
+        print(tracknames[ktrack])
 
-    track=[["doDO3d3"]]
-    track=satie
-    trackname="satie.wav"
-
-    # génération des notes (sur un set Python)
-    k=0
-    for note in set(tuple(i) for i in track) :
-        #print(note)
-        genNote(note,plotNote=False,writeWav=True)
-        k+=1
-    print(str(k)+" notes générés")
-
-    notenamefiles=''
-    # lecture
-    for notes in track:
-        notename=''
-        for note  in notes:
-            notename+=note
-        notenamefile=notename+".wav"
-        print(notenamefile)
-        notenamefiles+=notenamefile+' '
-        os.system("mplayer "+notenamefile+" > /dev/null 2>&1")    
-    os.system("sox "+notenamefiles+trackname)
+        # ----------------------------------------
+        # génération des notes (sur un set Python)
+        # ----------------------------------------
+        knote=0
+        setNotes=set(tuple(i) for i in track)
+        for note in setNotes :
+            genNote(note,plotNote=True,writeWav=True,verbose=11)
+        print(len(setNotes)," notes générés",set(tuple(i) for i in track))
+        notenamefiles=''
+        # ----------------------------------------
+        # lecture
+        for notes in track:
+            notename=''
+            for note  in notes:
+                notename+=note
+            notenamefile=notename+".wav"
+            print(notenamefile)
+            notenamefiles+=notenamefile+' '
+            os.system("mplayer "+notenamefile+" > /dev/null 2>&1")  # jouer la note dans l'ordre de la voix
+        os.system("sox "+notenamefiles+tracknames[ktrack]) # fichier wav de la voix (track)
+        tracknamefiles+=tracknames[ktrack]+" "
+    os.system("sox -m "+tracknamefiles+piecename) # 
+    os.system("mplayer "+piecename+" > /dev/null 2>&1")  # jouer tous le morceau 
 
 
     
